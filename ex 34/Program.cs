43077a716c4cc9384f89670413d5daf01d1f2c6a
alpryakh <alpryakh@gmail.com>
2022-11-12 15:08:46 +0300

    ex 34

---
+++ ex 34/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Enter your array's length:");
+int length = int.Parse(Console.ReadLine());
+int[] array = GetArray(length, 100, 1000);
+
+Console.WriteLine($"[ {String.Join(", ", array)} ] ");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int a = 0;
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue);
+
+        if (result[i] % 2 == 0)
+        {
+            a++;
+        }
+    }
+    Console.WriteLine(a);
+    return result;
+}
+
+
