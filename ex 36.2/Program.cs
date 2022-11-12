22747eecc0d3bedd2bfdfbe2917f3c0183a5db08
alpryakh <alpryakh@gmail.com>
2022-11-12 15:09:50 +0300

    ex 36.2

---
+++ ex 36.2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Enter your array's size:");
+int size = int.Parse(Console.ReadLine());
+int[] myArray = CreateRandomArray(size, -100, 100);
+ShowArray(myArray);
+
+int sum = 0;
+
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] % 2 == 1)
+        sum += myArray[i];
+
+}
+
+Console.Write($"{sum}");
+int[] CreateRandomArray(int N, int start, int end)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+void ShowArray(int[] array)
+{
+    Console.WriteLine($"[ {String.Join(", ", array)} ] => ");
+}
